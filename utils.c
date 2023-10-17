3666b9ee324b56e1bc15ee529c092227f633dbf8
Brook <sintayehubrook1540@gmail.com>
2023-10-17 11:23:38 -0700

    Update

---
+++ utils.c
@@ -0,0 +1,88 @@
+#include "main.h"
+
+/**
+ * evaluate_character - Determines if a character is displayable
+ * @c: The character to be assessed.
+ *
+ * Return: 1 if c is displayable, 0 otherwise.
+ */
+int is_printable(char c)
+{
+	if (c >= 32 && c < 127)
+		return (1);
+
+	return (0);
+}
+
+/**
+ * append_hex_code - Add ASCII in hexadecimal format to buffer
+ * @buffer: Array of characters.
+ * @index: Index to start appending.
+ * @ascii_value: ASCII value.
+ *
+ * Return: Always returns 3.
+ */
+int append_hexa_code(char ascii_code, char buffer[], int i)
+{
+	char map_to[] = "0123456789ABCDEF";
+	/* The hexa format code is always 2 digits long */
+	if (ascii_code < 0)
+		ascii_code *= -1;
+
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+
+	return (3);
+}
+
+/**
+ * check_if_digit - Checks if a character is a digit
+ * @c: Character to be examined
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+
+	return (0);
+}
+
+/**
+ * convert_size_number - Casts a number to the specified size
+ * @num: Number to be casted.
+ * @size: Number indicating the type to be casted.
+ *
+ * Return: Casted value of num
+ */
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+
+	return ((int)num);
+}
+
+/**
+ * convert_to_specified_type - Converts a number to the specified data type
+ * @num: Number to be converted
+ * @type: Data type indicator for the conversion
+ *
+ * Return: Converted value of num
+ */
+long int convert_size_unsgnd(unsigned long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+
+	return ((unsigned int)num);
+}
+
